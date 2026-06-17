29c05cd7f8f45a549b95067c190a145601eeee81
Felype <felype.g.ferreira@aluno.senai.br>
2026-06-17 13:50:30 -0300

    formatado os arquivos index.html e js/scripts_repticoes.js

--- js/scripts_repticoes.js
+++ js/scripts_repticoes.js
@@ -1,4 +1,35 @@
 //ESTRTURA DE REPETIÇÃO FOR
+const divFor = document.querySelector('#div-for')
+
 for(i = 0; i < 10; i++ ){
     console.log(i, 'Amor')
+    divFor.innerHTML += `${i} - Amor <br>`
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click' , (evet)=>{
+    let nmuDigitado = Number(inputNum.value)
+
+    cont++
+    acum += nmuDigitado
+
+    divContAcum.innerHTML = `TOTAL DE NÚMEROS DIGITADOS É: ${cont} <br>
+    A SOMA DOS NÚMEROS É: ${acum}`
+
+    inputNum.value = ''
+
+})
+
+//CONTROLADOR FOR
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
+
